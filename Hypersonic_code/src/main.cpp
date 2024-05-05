d4544aa8e89cf1a94140e8a9cb2b4e1f7aecf4a9
Adam Anderson <adaand00@gmail.com>
2024-05-05 10:39:15 +0200

    Add custom pin definitions

--- Hypersonic_code/src/main.cpp
+++ Hypersonic_code/src/main.cpp
@@ -1,31 +1,31 @@
 #include <Arduino.h>
 #include <lcdgfx.h>
 
-#define GPD1 15
-#define GPA0 4
-#define GPA1 5
-#define GPA2 6
-
-#define SPI_MISO 13
-#define SPI_MOSI 11
-#define SPI_CLK 12
-
-#define LCD_CS GPD1
-#define LCD_BL GPA0
-#define LCD_RES GPA1
-#define LCD_DC GPA2
+#define LCD_CS D1
+#define LCD_BL A0
+#define LCD_RES A1
+#define LCD_DC A2
 
 DisplayST7735_128x160x16_SPI disp(LCD_RES, 
-  {-1, LCD_CS, LCD_DC, 0, SPI_CLK, SPI_MOSI});
+  {-1, LCD_CS, LCD_DC, 0, SCK, MOSI});
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // Display setup
+  pinMode(LCD_BL, OUTPUT);
+  analogWrite(LCD_BL, 100);
+
   disp.begin();
   disp.getInterface().setRotation(1);
   disp.getInterface().setOffset(2,1);
   disp.clear();
-  disp.setFixedFont(ssd1306xled_font6x8);
+  disp.setFixedFont(ssd1306xled_font5x7);
+
   disp.printFixed(2,2,"Hello World!");
+
+  // Pot setup
+
+
 }
 
 void loop() {
---
+++ Hypersonic_code/variants/hypersonic/pins_arduino.h
@@ -0,0 +1,53 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+#define USB_VID          0x303a
+#define USB_PID          0x814D  // for user apps (https://github.com/espressif/usb-pids/pull/77)
+#define USB_MANUFACTURER "Adam"
+#define USB_PRODUCT      "Hypersonic"
+#define USB_SERIAL       ""
+
+// ADC enabled
+static const uint8_t A0 = 4;
+static const uint8_t A1 = 5;
+static const uint8_t A2 = 6;
+static const uint8_t A3 = 7;
+static const uint8_t A4 = 8;
+static const uint8_t A5 = 9;
+static const uint8_t A6 = 10;
+static const uint8_t A7 = 1;
+static const uint8_t A8 = 2;
+
+// No ADC
+static const uint8_t D0 = 14;
+static const uint8_t D1 = 15;
+static const uint8_t D2 = 16;
+static const uint8_t D3 = 17;
+static const uint8_t D4 = 18;
+
+// SPI
+static const uint8_t MISO = 13;
+static const uint8_t MOSI = 11;
+static const uint8_t SCK  = 12;
+
+static const uint8_t SS   = 10;
+
+// I2C (Pull-ups on board)
+static const uint8_t SDA  = 47;
+static const uint8_t SCL  = 48;
+
+// Codec pins
+static const uint8_t I2S_DI   = 38;
+static const uint8_t I2S_DO   = 39;
+static const uint8_t I2S_WCLK = 40;
+static const uint8_t I2S_BCLK = 41;
+static const uint8_t I2S_MCLK = 42;
+static const uint8_t CODEC_EN = 41;
+
+// UART
+static const uint8_t TX = 43;
+static const uint8_t RX = 44;
+
+#endif /* Pins_Arduino_h */
